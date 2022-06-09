1309d94343540f7422c113dd7067fe438de59d8b
Victoria Igbobi <victoriaigbobi65@gmail.com>
2022-06-09 19:59:16 +0100

    completed function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,24 @@
 // complete the function
 function prime (num) {
-  // code goes here
+  if (num===1)
+  {
+    return false;
+  }
+  else if(num === 2)
+  {
+    return true;
+  }else
+  {
+    for(var i = 2; i < num; i++)
+    {
+      if(num % i === 0)
+      {
+        return false;
+      }
+    }
+    return true;  
+  }
+
 }
 
 function solution (arg) {
